a683e5a296ffc5ba5b69fdf0e2c6ca6d2d592db7
William Wong <souleni5k@Williams-MacBook-Pro-2.local>
2016-09-17 15:30:43 -0400

    code to start second activity

--- Moodometer/app/src/main/java/hackmit2016/moodometer/MainActivity.java
+++ Moodometer/app/src/main/java/hackmit2016/moodometer/MainActivity.java
@@ -2,6 +2,11 @@ package hackmit2016.moodometer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, Suggestions.class);
+        startActivity(intent);
     }
 }
